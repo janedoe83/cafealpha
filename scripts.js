e3a342c630dc51070b9e04d8bfaab38e593008ef
janedoe83 <marcel.stern83@proton.me>
2026-02-13 12:53:17 +0100

    add quarto page

---
+++ scripts.js
@@ -0,0 +1,2 @@
+<script src="https://unpkg.com/lite-youtube-embed@0.3.0/src/lite-yt-embed.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/lite-youtube-embed@0.3.0/src/lite-yt-embed.css" />
